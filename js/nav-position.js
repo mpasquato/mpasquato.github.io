67d1a1dc8a823bb46148455e46e478ea56fe9b71
mpasquato <63077221+mpasquato@users.noreply.github.com>
2023-12-02 19:12:32 +0100

    Main update (header and general fixes)
    - optimized files structure
    - cleaned code
    - added navbar, with intended positioning and toggle b/w desktop and mobile (mobile navbar still to be created)

---
+++ js/nav-position.js
@@ -0,0 +1,26 @@
+const navDesktop = document.getElementById("nav-desktop");
+const navMobile = document.getElementById("nav-mobile");
+const hero = document.getElementById("hero");
+
+// Onscroll, check the position of the nav bars; if necessary, toggle them to sticky
+window.onscroll = function () 
+{
+    toggleNav(navDesktop);
+    toggleNav(navMobile);
+};
+
+function toggleNav(nav) 
+{
+    if (document.documentElement.scrollTop > hero.offsetHeight) {
+        nav.classList.add("fix-nav");
+    } else {
+        nav.classList.remove("fix-nav");
+    }
+}
+
+// Utility: calculate vh in pixels
+function vh(percent) 
+{
+    let h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    return (percent * h) / 100;
+}
---
+++ js/nav-responsive.js
@@ -0,0 +1,22 @@
+const navDesktop = document.getElementById("nav-desktop");
+const navMobile = document.getElementById("nav-mobile");
+
+// Run on loading and on resize
+window.addEventListener('load', checkWidth);
+window.addEventListener('resize', checkWidth);
+
+// Check viewport width; if necessary, toggle nav bar visibility
+function checkWidth() {
+    let viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+
+    if (viewportWidth >= 768) 
+    {
+        navDesktop.style.display = "block";
+        navMobile.style.display = "none";
+    } 
+    else
+    {
+        navDesktop.style.display = "none";
+        navMobile.style.display = "block";
+    }
+}
